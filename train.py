70370e0f63bf221259989f7fc621964d40ec5055
Morales97 <danimoralesbrotons@gmail.com>
2021-12-11 16:48:39 +0100

    save and load models

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from one_hot_encoding import get_dataset_one_hot
 from multi_hot_encoding import get_dataset_multi_hot
 from models.lstm_chord_models import LSTMChord, LSTMChordEmbedding, LSTMChordEmbedding_Multihot
 from models.lstm_melody_models import LSTM_Multihot, LSTM_Multihot_MLP
+from argparse import ArgumentParser
 import pdb
 
 # Andrew's
@@ -72,7 +73,7 @@ def evaluate_model(model, dataset):
     return acc
          
 
-if __name__ == "__main__":
+def train(args):
     # Get dataset
     # 354 training samples
     batch_size = 20
@@ -93,7 +94,7 @@ if __name__ == "__main__":
     #optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.00001)
     #optimizer = optim.SGD(model.parameters(), lr=0.0005)
     #optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
-    epochs = 3
+    epochs = 1
     train_losses = []
     val_losses = []
     train_accuracies = []
@@ -156,3 +157,50 @@ if __name__ == "__main__":
     plt.legend()
     plt.savefig('acc.png')
     plt.show()
+
+    if args.save_path is not None:
+        torch.save(model.state_dict(), args.save_path)
+
+def load_model(load_path):
+    train_dataset, val_dataset, test_dataset, input_size, target_size = get_dataset_multi_hot(choice=4)
+
+    # Create model
+    #model = LSTMChord(vocab_size, lstm_hidden_size=16)
+    #model = LSTMChordEmbedding(vocab_size, embed_size=16, lstm_hidden_size=16)
+    model = LSTM_Multihot(input_size, embed_size=64, lstm_hidden_size=64, target_size=target_size, num_layers=2, dropout_linear=0.4, dropout_lstm=0.4)
+    #model = LSTM_Multihot_MLP(input_size, embed_size=64, lstm_hidden_size=64, target_size=target_size, num_layers=2, dropout_linear=0.4, dropout_lstm=0.4)
+
+    model.load_state_dict(torch.load(load_path))
+
+    # EVALUATE
+    tr_acc = evaluate_model(model, train_dataset)
+    print('Train accuracy:\t%.2f' % tr_acc)
+    val_acc = evaluate_model(model, val_dataset)
+    print('Val accuracy:\t%.2f' % val_acc)
+    te_acc = evaluate_model(model, test_dataset)
+    print('Test accuracy:\t%.2f' % te_acc)
+
+
+
+
+if __name__ == "__main__":
+    np.random.seed(42)
+
+    parser = ArgumentParser(description='Train a model')
+    parser.add_argument('--save-path', type=str,
+                        # required=True,
+                        #default=None,
+                        default='models/trained_models/model_name.pth',
+                        help='')
+    parser.add_argument('--load-path', type=str,
+                        # required=True,
+                        #default=None,
+                        default='models/trained_models/model_name.pth',
+                        help='')
+
+    args = parser.parse_args()
+
+    if args.load_path is not None:
+        load_model(args.load_path)
+    else:
+        train(args)
